f6c848c6117aa3b56d8efb3c0fac2cc0e68060ef
pedroooheg <pedrohenriquegomes2@hotmail.com>
2024-11-10 21:41:13 -0300

    Initial commit

---
+++ PedagioEletronico.py
@@ -0,0 +1,6 @@
+class PedagioEletronico:
+    def __init__(self, numeroCabine, status, horario):
+        self.numeroCabine = numeroCabine
+        self.status = status
+        self.horario = horario
+
